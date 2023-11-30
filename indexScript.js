7b7a4c4afa4e08b6169daa8a11c309fa19a4bcb8
apayan09 <144732130+apayan09@users.noreply.github.com>
2023-11-30 13:30:20 -0700

    Update indexScript.js

--- indexScript.js
+++ indexScript.js
@@ -3,5 +3,10 @@ console.log("I hope you enjoy learning JavaScript!");
 
 function changetext(){
 document.getElementById("demo").innerHTML="hello"
+ console.log(nameOfElement);
+}
+
+function printToConsole(input) {
+    window.alert(input);
 }
 
